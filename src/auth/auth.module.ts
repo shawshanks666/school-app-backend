6f0f2b622a0d6623a746efdb3102e7c00f32d183
shashank <111701921+shawshanks666@users.noreply.github.com>
2025-09-17 04:25:52 +0530

    updated jwt expiration time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
-          expiresIn: '1d',
+          expiresIn: configService.get<string>('JWT_EXPIRATION'),
         },
       }),
     }),
